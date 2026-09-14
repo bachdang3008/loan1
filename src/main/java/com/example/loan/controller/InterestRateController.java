6ce29bbc31e6defc054ec9c3b09552476d90ce5c
bachdang3008 <bdang30082005@gmail.com>
2026-09-14 23:39:00 +0700

    Update InterestRateController.java

--- src/main/java/com/example/loan/controller/InterestRateController.java
+++ src/main/java/com/example/loan/controller/InterestRateController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/interestrate")
+@RequestMapping("/intessasasrestrate")
 public class InterestRateController {
     public final InterestRateService interestrateService;
 
